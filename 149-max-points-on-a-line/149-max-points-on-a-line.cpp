d9bdcffb9c1e03234ef5330c4e9df332f126ad7c
joshia18 <87872857+joshia18@users.noreply.github.com>
2022-04-27 16:18:05 +0530

    Time: 34 ms (36.79%), Space: 13.4 MB (19.75%) - LeetHub

--- 149-max-points-on-a-line/149-max-points-on-a-line.cpp
+++ 149-max-points-on-a-line/149-max-points-on-a-line.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+    
+    int getgcd(int x, int y){
+        return y == 0 ? x : getgcd(y, x%y);
+    }
+    
+    double getslope(vector<int> &p1, vector<int> &p2){
+        int x = p2[0] - p1[0];
+        int y = p2[1] - p1[1];
+        
+        int gcd = getgcd(x, y);
+        
+        x = gcd == 0 ? x / max(x, y) : x/gcd;
+        y = gcd == 0 ? y / max(x, y) : y/gcd;
+        
+        return (double)x/y;
+    }
+    
     int maxPoints(vector<vector<int>>& points) {
         //if the input contains only two points, then simply return points.size()
         
@@ -9,19 +26,13 @@ public:
         
         int maxpoints = 2;
         
-        for(int i = 0; i < points.size(); i++){
+        for(int i = 0; i < points.size()-1; i++){
             unordered_map<double, int> slopecount;
             int currMax = 0;
             
-            for(int j = 0; j < points.size(); j++){
+            for(int j = i+1; j < points.size(); j++){
                 
-                if(i == j) continue;
-                
-                //slope formula y2-y1/x2-x1
-                int nume = (points[j][1] - points[i][1]);
-                int deno = (points[j][0] - points[i][0]);
-                
-                double slope = (double)nume/deno;
+                double slope = getslope(points[i], points[j]);
                 
                 slopecount[slope]++;
                 
