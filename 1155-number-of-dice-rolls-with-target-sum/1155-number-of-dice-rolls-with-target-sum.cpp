1b46df1744999db41348113061844000ce3680da
joshia18 <87872857+joshia18@users.noreply.github.com>
2022-05-02 00:02:56 +0530

    Time: 67 ms (34.77%), Space: 8.4 MB (44.76%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -2,27 +2,30 @@ class Solution {
 public:
     //using memoization
     int MOD = 1000000007;
-    unordered_map<string, int> hashmap;
+    
     
     int numRollsToTarget(int n, int k, int target) {
         
-        //base cases
-        if(target < n || target > (n*k)) return 0;
+        //using dp
+        vector<vector<int>> dp(n, vector<int>(target+1, 0));
         
-        if(n == 1) return target <= k ? 1 : 0;
-        
-        string key = to_string(n) + "," + to_string(k) + "," + to_string(target);
-        
-        if(hashmap.find(key) != hashmap.end()) return hashmap[key];
-        
-        int sum = 0;
-        for(int i = 1; i <= k; i++){
-            //reduce number of dice by one while calling recursion
-            sum += numRollsToTarget(n-1, k, target-i);
-            sum %= MOD;
+        for(int i = 1; i <= min(target, k); i++){
+            dp[0][i] = 1;
         }
         
-        return hashmap[key] = sum;
+        for(int dice = 1; dice < n; dice++){
+            for(int total = dice; total <= min(target, (dice+1) * k); total++){
+                for(int faces = 1; faces <= k; faces++){
+                    
+                    if(total - dice < faces) continue;
+                    
+                    dp[dice][total] += dp[dice-1][total - faces];
+                    dp[dice][total] %= MOD;
+                }
+            }
+        }
+        
+        return dp[n-1][target];
         
     }
 };
