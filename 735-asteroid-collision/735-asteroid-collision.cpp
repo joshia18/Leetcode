dad64444c4e71952b3e637c3aaaf331c60b0fe71
joshia18 <87872857+joshia18@users.noreply.github.com>
2022-05-03 04:35:43 +0530

    Time: 13 ms (78.11%), Space: 17.7 MB (60.18%) - LeetHub

---
+++ 735-asteroid-collision/735-asteroid-collision.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        //so solved using stack
+        
+        vector<int> ans;
+        stack<int> stk;
+        
+        for(int i : asteroids){
+            
+            //we don't consider i == 0, only >0 and <0
+            if(i > 0){
+                //if asteroids go in right direction just add it to the stack
+                stk.push(i);
+            }
+            else if(i < 0){
+                //if it moves in the left, do the below operation
+                
+                //IMPORTANT: this blow variable keeps track if the current i asteroid is blasted or not
+                bool blow = false;
+                
+                while(!stk.empty()){
+                    //don't forget -i here
+                    if(stk.top() > -i){
+                        blow = true;
+                        break;
+                    }
+                    if(stk.top() == -i){
+                        blow = true;
+                        stk.pop();
+                        break;
+                    }
+                    if(stk.top() < -i){
+                        stk.pop();
+                    }
+                }
+                
+                //if blow is false, the current asteroid i has not bloasted, add it to the ans array
+                if(blow == false) ans.push_back(i);
+            }
+            
+        }
+        
+        int size = ans.size();
+        
+        while(!stk.empty()){
+            //insert at the beginning
+            ans.insert(ans.begin()+size, stk.top());
+            stk.pop();
+        }
+        
+        
+        return ans;
+        
+    }
+};
