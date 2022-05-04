f7bbbea8dc42b128a8ce67cdf83aa8589283189c
joshia18 <87872857+joshia18@users.noreply.github.com>
2022-05-04 17:36:22 +0530

    Time: 3 ms (99.82%), Space: 11.3 MB (71.18%) - LeetHub

---
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    
+       
+    vector<string> reorderLogFiles(vector<string>& logs) {
+        //sort(logs.begin(), logs.end(), comp);
+        
+        vector<pair<string, string>> letterlogs;
+        vector<string> digitlogs;
+        
+        for(auto log : logs){
+            int i = 0;
+            while(log[i] != ' ') i++;
+            
+            if(log[i+1] >= '0' && log[i+1] <= '9'){
+                digitlogs.push_back(log.substr(0, i) + log.substr(i));
+                cout << "digitlogs: " << digitlogs.back() << endl;
+            }
+            else{
+                letterlogs.push_back({log.substr(i), log.substr(0, i)});
+            }
+        }
+        
+        sort(letterlogs.begin(), letterlogs.end());
+        
+        vector<string> ans;
+        
+        for(auto i : letterlogs) ans.push_back(i.second + i.first);
+        for(auto i : digitlogs) ans.push_back(i);
+        
+        return ans;
+    }
+};
