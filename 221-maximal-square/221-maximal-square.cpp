ce7287916345b4237c76a2e619ecc49325cb6a7a
joshia18 <87872857+joshia18@users.noreply.github.com>
2022-05-12 18:16:58 +0530

    Time: 87 ms (45.28%), Space: 18.1 MB (64.21%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -5,40 +5,28 @@ public:
         
         if(r == 0) return 0;
         
-        //improved space complexity
-        vector<vector<int>> dp(2, vector<int>(c));
-        
         int ans = 0;
         
+        vector<vector<int>> dp(r, vector<int>(c, 0));
+        
         for(int i = 0; i < r; i++){
-            
-            if(i > 1){
-                for(int k = 0; k < c; k++){
-                    dp[0][k] = dp[1][k];
-                    //dp[1][k] = 0;
-                }
-            }
-                        
             for(int j = 0; j < c; j++){
-                           
                 if(i == 0){
-                    dp[0][j] = matrix[i][j] == '1' ? 1 : 0;
-                    ans = max(ans, dp[0][j]);
+                    dp[i][j] = matrix[i][j] == '1' ? 1 : 0;
                 }
                 else if(j == 0){
-                    dp[1][j] = matrix[i][j] == '1' ? 1 : 0;
+                    dp[i][j] = matrix[i][j] == '1' ? 1 : 0;
                 }
                 else if(matrix[i][j] == '1'){
-                    dp[1][j] = 1 + min({dp[0][j], dp[1][j-1], dp[0][j-1]});
+                    dp[i][j] = 1 + min({dp[i-1][j-1], dp[i-1][j], dp[i][j-1]});
                 }
                 else{
-                    dp[1][j] = 0;
+                    dp[i][j] = 0;
                 }
                 
-                ans = max(ans, dp[1][j]);
+                ans = max(ans, dp[i][j]);
                 
-            }
-            
+            }                
         }
         
         return ans*ans;
