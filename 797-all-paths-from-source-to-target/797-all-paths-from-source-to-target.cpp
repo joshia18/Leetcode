3480d81b632c995bc9cbae800ec9b953a1c59672
joshia18 <87872857+joshia18@users.noreply.github.com>
2022-05-13 15:22:59 +0530

    Time: 31 ms (35.68%), Space: 15.3 MB (35.84%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -2,21 +2,22 @@ class Solution {
 public:
     
     void dfs(vector<vector<int>>& graph, int start, int end, vector<vector<int>> &ans, vector<int> current){
+        current.push_back(start);
         if(start == end){
             ans.push_back(current);
         }
         
-        if(current.size() == 0) current.push_back(start);
+        //if(current.size() == 0) current.push_back(start);
         
         for(int i : graph[start]){
-            current.push_back(i);
+            //current.push_back(i);
             dfs(graph, i, end, ans, current);
-            current.pop_back();
+            //current.pop_back();
         }
     }
     
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        //using dfs
+        //using dfs which does not use backtracking, this problem was solved using three methods
         vector<vector<int>> ans;
         int n = graph.size();
         vector<int> current;
