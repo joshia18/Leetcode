cc788f4ff2052393f2faebc1d142328189edda4f
joshia18 <87872857+joshia18@users.noreply.github.com>
2022-05-14 13:54:46 +0530

    Time: 25 ms (19.90%), Space: 11.2 MB (73.30%) - LeetHub

--- 937-reorder-data-in-log-files/937-reorder-data-in-log-files.cpp
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.cpp
@@ -13,7 +13,7 @@ public:
             while(log[i] != ' ') i++;
             
             if(log[i+1] >= '0' && log[i+1] <= '9'){
-                digitlogs.push_back(log.substr(0, i) + log.substr(i));
+                digitlogs.push_back(log);
                 cout << "digitlogs: " << digitlogs.back() << endl;
             }
             else{
