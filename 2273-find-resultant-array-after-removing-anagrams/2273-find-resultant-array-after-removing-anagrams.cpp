3948de7a9cf593006e9cac3159bb606076697c1b
joshia18 <87872857+joshia18@users.noreply.github.com>
2022-05-19 23:00:02 +0530

    Time: 85 ms (5.03%), Space: 20.4 MB (5.23%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
@@ -3,11 +3,19 @@ public:
     vector<string> removeAnagrams(vector<string>& words) {
         
         for(int i = 1; i < words.size();){
-            string a = words[i];
-            string b = words[i-1];
+            map<char, int> a, b;
             
-            sort(a.begin(), a.end());
-            sort(b.begin(), b.end());
+            for(char c : words[i]){
+                a[c]++;
+            }
+            
+            for(char c : words[i-1]){
+                b[c]++;
+            }
+            
+            //instead of sorting we can use ordered map also
+            // sort(a.begin(), a.end());
+            // sort(b.begin(), b.end());
             
             if(a == b){
                 words.erase(words.begin()+i);
