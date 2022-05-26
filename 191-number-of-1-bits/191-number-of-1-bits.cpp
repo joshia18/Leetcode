8722f05edaa583f6d66755e4b57614a3c0f745d5
joshia18 <87872857+joshia18@users.noreply.github.com>
2022-05-27 01:35:44 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (48.75%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -4,8 +4,8 @@ public:
         int ans = 0;
         
         while(n > 0){
-            if(n%2) ans++;
-            n = n/2;
+            n = n & (n-1);
+            ans++;
         }
         
         return ans;
