89fd2ca6bc659a439ca90eae05c3a985b18fd5cc
joshia18 <87872857+joshia18@users.noreply.github.com>
2022-06-14 00:08:10 +0530

    Time: 20 ms (68.43%), Space: 8.7 MB (33.88%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,28 +1,45 @@
 class Solution {
 public:
-    int helper(int i, vector<int>& arr, int k, vector<int> &dp){
-        if(i == arr.size()) return 0;
+//     int helper(int i, vector<int>& arr, int k, vector<int> &dp){
+//         if(i == arr.size()) return 0;
         
-        if(dp[i] != -1) return dp[i];
+//         if(dp[i] != -1) return dp[i];
         
-        //j < i+k is the maximum partition that we can do
-        int len = 0, maxelement = INT_MIN, maxsum = INT_MIN;
+//         //j < i+k is the maximum partition that we can do
+//         int len = 0, maxelement = INT_MIN, maxsum = INT_MIN;
         
-        for(int j = i; j < min(i+k, (int)arr.size()); j++){
-            len++;
-            maxelement = max(maxelement, arr[j]);
-            int sum = maxelement * len + helper(j+1, arr, k, dp);
-            maxsum = max(maxsum, sum);
-        }
+//         for(int j = i; j < min(i+k, (int)arr.size()); j++){
+//             len++;
+//             maxelement = max(maxelement, arr[j]);
+//             int sum = maxelement * len + helper(j+1, arr, k, dp);
+//             maxsum = max(maxsum, sum);
+//         }
         
-        return dp[i] = maxsum;
-    }
+//         return dp[i] = maxsum;
+//     }
     
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
         
-        vector<int> dp(n, -1);
+        vector<int> dp(n+1, 0);
         
-        return helper(0, arr, k, dp);
+        dp[n] = 0;
+        
+        for(int i = n-1; i >= 0; i--){
+            
+            int len = 0, maxelement = INT_MIN, maxsum = INT_MIN;
+        
+            for(int j = i; j < min(i+k, (int)arr.size()); j++){
+                len++;
+                maxelement = max(maxelement, arr[j]);
+                int sum = maxelement * len + dp[j+1];
+                maxsum = max(maxsum, sum);
+            }
+            
+            dp[i] = maxsum;
+        }
+        
+        return dp[0];
+        //return helper(0, arr, k, dp);
     }
 };
