aace13ab177c011c0941de467b97bc1d860caf7c
joshia18 <87872857+joshia18@users.noreply.github.com>
2022-06-13 17:12:43 +0530

    Time: 572 ms (26.67%), Space: 6.5 MB (83.60%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -10,30 +10,49 @@ public:
         return true;
     }
     
-    int helper(int index, string &s, vector<int> &dp){
-        //base case
-        if(index == s.size()) return 0;
+//     int helper(int index, string &s, vector<int> &dp){
+//         //base case
+//         if(index == s.size()) return 0;
         
-        if(dp[index] != -1) return dp[index];
+//         if(dp[index] != -1) return dp[index];
         
-        int mincuts = INT_MAX;
+//         int mincuts = INT_MAX;
         
-        for(int i = index; i < s.size(); i++){
+//         for(int i = index; i < s.size(); i++){
             
-            if(isPalindrome(s, index, i)){
-                int cuts = 1 + helper(i+1, s, dp);
-                mincuts = min(mincuts, cuts);
-            }
-        }
+//             if(isPalindrome(s, index, i)){
+//                 int cuts = 1 + helper(i+1, s, dp);
+//                 mincuts = min(mincuts, cuts);
+//             }
+//         }
         
-        return dp[index] = mincuts;
-    }
+//         return dp[index] = mincuts;
+//     }
     
     int minCut(string s) {
         int n = s.size();
         
-        vector<int> dp(n, -1);
+        vector<int> dp(n+1, 0);
         
-        return helper(0, s, dp)-1;
+        //base case
+        dp[n] = 0;
+        
+        for(int i = n-1; i >= 0; i--){
+            int mincuts = INT_MAX;
+        
+            for(int j = i; j < s.size(); j++){
+
+                if(isPalindrome(s, i, j)){
+                    int cuts = 1 + dp[j+1];
+                    mincuts = min(mincuts, cuts);
+                }
+            }
+            
+            dp[i] = mincuts;
+        }
+        
+        return dp[0]-1;
+        
+        //return helper(0, s, dp)-1;
     }
 };
