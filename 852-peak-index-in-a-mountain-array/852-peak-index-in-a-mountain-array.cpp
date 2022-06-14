82b19b07fa9d5840c76a9502c334baa3d674ae3f
joshia18 <87872857+joshia18@users.noreply.github.com>
2022-06-15 00:44:50 +0530

    Time: 23 ms (10.93%), Space: 11.5 MB (90.05%) - LeetHub

---
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        //usin binary search fraz
+        
+        int start = 0, end = arr.size()-1, ans = -1;
+        
+        //here start < end, not start <= end because inorder to find peak element we need to compare two numbers
+        //mid and mid+1 for the left half, where mid+1 is the answer case (arr[mid] < arr[mid+1])
+        //mid and mid+1 for the right half, where mid is the answer case (arr[mid] > arr[mid+1])
+        while(start < end){
+            
+            int mid = start + (end-start)/2;
+            
+            if(arr[mid] > arr[mid+1]){
+                ans = mid;
+                end = mid;
+            }
+            else{
+                ans = mid+1;
+                start = mid+1;
+            }
+        }
+        
+        return ans;
+    }
+};
