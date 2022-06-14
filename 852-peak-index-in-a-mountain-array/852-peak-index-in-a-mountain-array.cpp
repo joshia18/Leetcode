30e7fdb03f56e760789fe84d935b0770b2654cf5
joshia18 <87872857+joshia18@users.noreply.github.com>
2022-06-15 00:45:15 +0530

    Time: 23 ms (10.93%), Space: 11.6 MB (49.37%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -8,6 +8,7 @@ public:
         //here start < end, not start <= end because inorder to find peak element we need to compare two numbers
         //mid and mid+1 for the left half, where mid+1 is the answer case (arr[mid] < arr[mid+1])
         //mid and mid+1 for the right half, where mid is the answer case (arr[mid] > arr[mid+1])
+        //end = mid because we don't want to miss the answer mid if that is the answer
         while(start < end){
             
             int mid = start + (end-start)/2;
