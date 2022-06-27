9f419bbbbfc4566f11a268c56adcc771cda354fb
joshia18 <87872857+joshia18@users.noreply.github.com>
2022-06-28 03:58:37 +0530

    Time: 25 ms (5.76%), Space: 9.1 MB (7.81%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
     
-    int helper(int i, int j, vector<vector<int>>& triangle, vector<vector<int>> &dp){
-        if(i == triangle.size()-1) return triangle[triangle.size()-1][j];
+//     int helper(int i, int j, vector<vector<int>>& triangle, vector<vector<int>> &dp){
+//         if(i == triangle.size()-1) return triangle[triangle.size()-1][j];
         
-        if(dp[i][j] != -1) return dp[i][j];
+//         if(dp[i][j] != -1) return dp[i][j];
         
-        int down = triangle[i][j] + helper(i+1, j, triangle, dp);
-        int diagonal = triangle[i][j] + helper(i+1, j+1, triangle, dp);
+//         int down = triangle[i][j] + helper(i+1, j, triangle, dp);
+//         int diagonal = triangle[i][j] + helper(i+1, j+1, triangle, dp);
         
-        return dp[i][j] = min(down, diagonal);
-    }
+//         return dp[i][j] = min(down, diagonal);
+//     }
     
     int minimumTotal(vector<vector<int>>& triangle) {
-        //we start recursion from start 0 for this question as it is fixed point
+        //we start recursion from start 0 for this question as it is fixed point, basically r and c has same valued
         int r = triangle.size(), c = triangle[r-1].size();
         
-        vector<vector<int>> dp(r, vector<int>(c, -1));
+        vector<vector<int>> dp(r, vector<int>(c, 0));
         
-        return helper(0, 0, triangle, dp);
+        for(int i = r-1; i >= 0; i--){
+            for(int j = triangle[i].size()-1; j >= 0; j--){
+                if(i == r-1) dp[r-1][j] = triangle[r-1][j];
+                else{
+                    
+                    int down = 1e6, diagonal = 1e6;
+                    if(i+1 <= r-1) down = triangle[i][j] + dp[i+1][j];
+                    if(j+1 <= c-1) diagonal = triangle[i][j] + dp[i+1][j+1];
+                    
+                    dp[i][j] = min(down, diagonal);
+                }
+            }
+        }
+        
+        return dp[0][0];
+        
+        //return helper(0, 0, triangle, dp);
     }
 };
