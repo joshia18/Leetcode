2cbf32743c20b28fff8a7289d6936d286e344365
joshia18 <87872857+joshia18@users.noreply.github.com>
2022-06-27 15:57:37 +0530

    Time: 3 ms (53.60%), Space: 7.6 MB (75.26%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,41 +1,45 @@
 class Solution {
 public:
-    vector<int> mem;
-    int helper(int index, vector<int>& nums){
-        if(index == 0) return nums[index];
-        if(index < 0) return 0;
+//     vector<int> mem;
+//     int helper(int index, vector<int>& nums){
+//         if(index == 0) return nums[index];
+//         if(index < 0) return 0;
         
-        if(mem[index] != -1) return mem[index];
+//         if(mem[index] != -1) return mem[index];
         
-        int pick = nums[index] + helper(index-2, nums);
-        int notpick = helper(index-1, nums);
+//         int pick = nums[index] + helper(index-2, nums);
+//         int notpick = helper(index-1, nums);
         
-        return mem[index] = max(pick, notpick);
+//         return mem[index] = max(pick, notpick);
         
-    }
+//     }
     
     int rob(vector<int>& nums) {
-//         int ans = 0;
-//         int dp[nums.size()];
         
-//         if(nums.size() == 1){
-//             return nums[0];
-//         }
+        if(nums.size() == 1) return nums[0];
         
-//         if(nums.size() == 2){
-//             return max(nums[0],nums[1]);
-//         }
+        int ans = 0;
         
-//         dp[0] = nums[0];
-//         dp[1] = max(nums[0],nums[1]);
+        int n = nums.size();
+        vector<int> dp(n, 0);
         
-//         for(int i = 2; i < nums.size(); i++){
-//             dp[i] = max(dp[i-1], nums[i] + dp[i-2]);
-//         }
+        dp[0] = nums[0];
         
-//         return dp[nums.size() - 1];
-        mem.resize(nums.size()+1, -1);
+        //start from 1
+        for(int i = 1; i < n; i++){
+            
+            int pick = nums[i];
+            if(i > 1) pick +=  dp[i-2];
+            
+            int notpick = dp[i-1];
+
+            dp[i] = max(pick, notpick);
+        }
         
-        return helper(nums.size()-1, nums);
+        return dp[n-1];
+        
+        //mem.resize(nums.size()+1, -1);
+        
+        //return helper(nums.size()-1, nums);
     }
 };
