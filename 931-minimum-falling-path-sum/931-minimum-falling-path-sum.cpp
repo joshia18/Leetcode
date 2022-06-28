f5d508dadc3edc0bbd726813d4686a3218e93bf3
joshia18 <87872857+joshia18@users.noreply.github.com>
2022-06-28 16:05:11 +0530

    Time: 30 ms (20.00%), Space: 9.8 MB (94.80%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -18,24 +18,28 @@ public:
         int r = matrix.size(), c = matrix[0].size();
         
         int ans = INT_MAX;
-        vector<vector<int>> dp(r, vector<int>(c, 0));
+        //vector<vector<int>> dp(r, vector<int>(c, 0));
+        vector<int> prev(c, 0), curr(c, 0);
         
-        //tabulation (for every possible i, value of j is 0 to c-1 as per the working)
+        //tabulation with space optimised (for every possible i, value of j is 0 to c-1 as per the working)
         //base case
-        for(int i = 0; i < c; i++) dp[0][i] = matrix[0][i];
+        for(int i = 0; i < c; i++) prev[i] = matrix[0][i];
         
         for(int i = 1; i < r; i++){
+            
             for(int j = 0; j < c; j++){
                 int s = 1e6, ld = 1e6, rd = 1e6;
-                s = matrix[i][j] + dp[i-1][j];
-                if(j-1 >= 0) ld = matrix[i][j] + dp[i-1][j-1];
-                if(j+1 < c) rd = matrix[i][j] + dp[i-1][j+1];
+                s = matrix[i][j] + prev[j];
+                if(j-1 >= 0) ld = matrix[i][j] + prev[j-1];
+                if(j+1 < c) rd = matrix[i][j] + prev[j+1];
                 
-                dp[i][j] = min(s, min(ld, rd));
+                curr[j] = min(s, min(ld, rd));
             }
+            
+            prev = curr;
         }
         
-        for(int i = 0; i < c; i++) ans = min(ans, dp[r-1][i]);
+        for(int i = 0; i < c; i++) ans = min(ans, prev[i]);
         
         // for(int i = 0; i < c; i++){
         //     ans = min(ans, helper(r-1, i, matrix, dp));
