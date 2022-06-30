219666c74e5619c018abd7c0668ce9d7ec551a74
joshia18 <87872857+joshia18@users.noreply.github.com>
2022-06-30 18:24:40 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (44.88%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int helper(int i, string &s, vector<int> &dp){
-        //base case
-        if(i >= s.size()) return 1;
+//     int helper(int i, string &s, vector<int> &dp){
+//         //base case
+//         if(i >= s.size()) return 1;
         
-        if(dp[i] != -1)  return dp[i];
+//         if(dp[i] != -1)  return dp[i];
         
-        int ans = 0;
+//         int ans = 0;
         
-        int one = s[i] - '0', two = 0;
-        if(i+1 < s.size()){
-            two = one*10 + s[i+1] - '0';
-        }
+//         int one = s[i] - '0', two = 0;
+//         if(i+1 < s.size()){
+//             two = one*10 + s[i+1] - '0';
+//         }
         
-        if(one > 0) ans += helper(i+1, s, dp);
-        if(one > 0 && two > 0 && two <= 26) ans += helper(i+2, s, dp);
+//         if(one > 0) ans += helper(i+1, s, dp);
+//         if(one > 0 && two > 0 && two <= 26) ans += helper(i+2, s, dp);
         
-        return dp[i] = ans;
-    }
+//         return dp[i] = ans;
+//     }
     
     int numDecodings(string s) {
         
@@ -25,8 +25,27 @@ public:
         
         int n = s.size();
         
-        vector<int> dp(n, -1);
+        vector<int> dp(n+2, -1);
+        //base case
+        dp[n] = dp[n+1] = 1;
         
-        return helper(0, s, dp);
+        //tabulation
+        for(int i = n-1; i >= 0; i--){
+            int ans = 0;
+        
+            int one = s[i] - '0', two = 0;
+            if(i+1 < s.size()){
+                two = one*10 + s[i+1] - '0';
+            }
+
+            if(one > 0) ans += dp[i+1];
+            if(one > 0 && two > 0 && two <= 26) ans += dp[i+2];
+
+            dp[i] = ans; 
+        }
+        
+        return dp[0];
+        
+        //return helper(0, s, dp);
     }
 };
