8c176865041f2dfcb3d57d2aefcb42cabef34ae0
joshia18 <87872857+joshia18@users.noreply.github.com>
2022-07-02 02:53:57 +0530

    Time: 164 ms (80.17%), Space: 60.8 MB (33.45%) - LeetHub

---
+++ 1472-design-browser-history/1472-design-browser-history.cpp
@@ -0,0 +1,56 @@
+struct node{
+    string url;
+    node* prev;
+    node* next;
+    node(string url) : url(url), prev(nullptr), next(nullptr) {}
+};
+
+class BrowserHistory {
+public:
+    node* head = new node("head");
+    node* tail = new node("tail");
+    node* currnode;
+    
+    BrowserHistory(string homepage) {
+        node* newnode = new node(homepage);
+        newnode->prev = head;
+        newnode->next = tail;
+        currnode = newnode;
+    }
+    
+    void visit(string url) {
+        node* newnode = new node(url);
+        //setting newnode's next and prev
+        //connecting newnode to pointer
+        newnode->next = tail;
+        currnode->next = newnode;
+        newnode->prev = currnode;
+        currnode = newnode;
+    }
+    
+    string back(int steps) {
+        while((currnode->prev->prev) && steps > 0 && currnode->prev != head){
+            currnode = currnode->prev;
+            steps--;
+        }
+        
+        return currnode->url;
+    }
+    
+    string forward(int steps) {
+        while((currnode->next->next) && steps > 0 && currnode->next != tail){
+            currnode = currnode->next;
+            steps--;
+        }
+        
+        return currnode->url;
+    }
+};
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory* obj = new BrowserHistory(homepage);
+ * obj->visit(url);
+ * string param_2 = obj->back(steps);
+ * string param_3 = obj->forward(steps);
+ */
