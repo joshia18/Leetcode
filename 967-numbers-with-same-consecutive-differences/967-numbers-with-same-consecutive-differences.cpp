697f21e31baa199756566fb422c9b948feac978c
joshia18 <87872857+joshia18@users.noreply.github.com>
2022-07-06 15:31:01 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (33.72%) - LeetHub

---
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void helper(int i, int n, int k, vector<int> &ans){
+        if(n == 0){
+            ans.push_back(i);
+            return;
+        }
+        
+        int lastnum = i%10;
+        
+        if(lastnum + k < 10){
+            helper(i * 10 + (lastnum+k), n-1, k, ans);
+        }
+        
+        //if k == 0 only one of the recursive calls should be called
+        if(k > 0 && lastnum-k >= 0){
+            helper(i * 10 + (lastnum-k), n-1, k, ans);
+        }
+    }
+    
+    vector<int> numsSameConsecDiff(int n, int k) {
+        vector<int> ans;
+        
+        for(int i = 1;i <= 9; i++){
+            helper(i, n-1, k, ans);
+        }
+        
+        return ans;
+    }
+};
