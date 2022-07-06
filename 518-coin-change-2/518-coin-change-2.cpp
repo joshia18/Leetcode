2fe10fb73acf6e11b9766fa9f18341ff82578bd2
joshia18 <87872857+joshia18@users.noreply.github.com>
2022-07-06 17:08:27 +0530

    Time: 43 ms (45.05%), Space: 7.2 MB (79.60%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -21,25 +21,26 @@ public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
         
-        vector<vector<int>> dp(n, vector<int>(amount+1, 0));
+        //vector<vector<int>> dp(n, vector<int>(amount+1, 0));
+        vector<int> prev(amount+1, 0);
         
         //here it is i%coins[0] not amount%coins[0] -> IMPORTANT
-        for(int i = 0; i <= amount; i++) dp[0][i] = i%coins[0] == 0 ? 1 : 0;
+        for(int i = 0; i <= amount; i++) prev[i] = i%coins[0] == 0 ? 1 : 0;
         
         //for all amount = 0, set all the rows to 1
-        for(int i = 0; i < n; i++) dp[i][0] = 1;
+        //for(int i = 0; i < n; i++) prev[0] = 1;
         
         for(int i = 1; i < n; i++){
             for(int j = 0; j <= amount; j++){
                 int take = 0;
-                if(j >= coins[i]) take = dp[i][j-coins[i]];
-                int nottake = dp[i-1][j];
+                if(j >= coins[i]) take = prev[j-coins[i]];
+                int nottake = prev[j];
 
-                dp[i][j] = take+nottake;
+                prev[j] = take+nottake;
             }
         }
         
-        return dp[n-1][amount];
+        return prev[amount];
         
         //return helper(n-1, amount, coins, dp);
     }
