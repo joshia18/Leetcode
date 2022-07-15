9ae7bf48d2e1d0f51eeb9fedac113aae6768b2b6
joshia18 <87872857+joshia18@users.noreply.github.com>
2022-07-16 01:48:53 +0530

    Time: 578 ms (21.12%), Space: 7.6 MB (42.83%) - LeetHub

---
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int helper(int i, int j, vector<vector<int>>& grid, vector<vector<int>> &visited){
+        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == 0 || visited[i][j]){
+            return 0;
+        }
+        
+        visited[i][j] = 1;
+        
+        int up = -1e6, down = -1e6, right = -1e6, left = -1e6;
+        
+        up = grid[i][j] + helper(i-1, j, grid, visited);
+        down = grid[i][j] + helper(i+1, j, grid, visited);
+        right = grid[i][j] + helper(i, j+1, grid, visited);
+        left = grid[i][j] + helper(i, j-1, grid, visited);
+        
+        visited[i][j] = 0;
+        
+        
+        return max({up, down, right, left});
+    }
+    
+    int getMaximumGold(vector<vector<int>>& grid) {
+        int ans = INT_MIN;
+        
+        int r = grid.size(), c = grid[0].size();
+        vector<vector<int>> visited(r, vector<int>(c, 0));
+        
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                if(grid[i][j] != 0){
+                    int temp = helper(i, j, grid, visited);
+                    ans = max(ans, temp);
+                }
+            }
+        }
+        
+        return ans < 0 ? 0 : ans;
+    }
+};
