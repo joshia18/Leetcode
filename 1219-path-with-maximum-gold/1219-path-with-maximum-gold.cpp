1d93829cc373703218c9d12f0ceeec7b3c84c1b8
joshia18 <87872857+joshia18@users.noreply.github.com>
2022-07-16 02:42:39 +0530

    Time: 453 ms (44.46%), Space: 7.5 MB (76.17%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-    int helper(int i, int j, vector<vector<int>>& grid, vector<vector<int>> &visited){
-        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == 0 || visited[i][j]){
+    int helper(int i, int j, vector<vector<int>>& grid){
+        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == 0){
             return 0;
         }
         
-        visited[i][j] = 1;
+        int temp = grid[i][j];
+        
+        grid[i][j] = 0;
         
         int up = -1e6, down = -1e6, right = -1e6, left = -1e6;
         
-        up = grid[i][j] + helper(i-1, j, grid, visited);
-        down = grid[i][j] + helper(i+1, j, grid, visited);
-        right = grid[i][j] + helper(i, j+1, grid, visited);
-        left = grid[i][j] + helper(i, j-1, grid, visited);
+        //don't forget to use temp instead of grid[i][j] while doing memory optimization of visited array
+        up = temp + helper(i-1, j, grid);
+        down = temp + helper(i+1, j, grid);
+        right = temp + helper(i, j+1, grid);
+        left = temp + helper(i, j-1, grid);
         
         //this visited[i][j] = 0 is mandatory here as it is returned from this particular place, it needs to be marked as not visited
-        visited[i][j] = 0;
+        grid[i][j] = temp;
         
         return max({up, down, right, left});
     }
@@ -25,12 +28,13 @@ public:
         
         int r = grid.size(), c = grid[0].size();
         
+        //there is an answer without using the visited array also, just make the grid value to zero whenever visiting and hchange it back to original value once the recurisin is completed
+        //vector<vector<int>> visited(r, vector<int>(c, 0));
         
         for(int i = 0; i < r; i++){
             for(int j = 0; j < c; j++){
                 if(grid[i][j] != 0){
-                    vector<vector<int>> visited(r, vector<int>(c, 0));
-                    int temp = helper(i, j, grid, visited);
+                    int temp = helper(i, j, grid);
                     ans = max(ans, temp);
                 }
             }
