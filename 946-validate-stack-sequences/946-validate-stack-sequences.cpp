4e25d3dd7426d48f2c3b2aacb2a5643d68b741c5
joshia18 <87872857+joshia18@users.noreply.github.com>
2022-07-17 00:14:34 +0530

    Time: 21 ms (18.39%), Space: 15.2 MB (83.52%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.cpp
+++ 946-validate-stack-sequences/946-validate-stack-sequences.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-        stack<int> stk;
-        int n = pushed.size();
+        //another solution without using the stack ds
+        //we achieved constant space by using and modifying the pushed input array itseld
         
         int i = 0, j = 0;
         
-        while(i < n){
-            stk.push(pushed[i]);
-            i++;
-            while(!stk.empty() && stk.top() == popped[j] &&  j < n){
-                stk.pop();
+        for(int &x : pushed){
+            pushed[i++] = x;
+            
+            while(i > 0 && pushed[i-1] == popped[j]){
+                i--;
                 j++;
             }
         }
         
-        return stk.empty() ? true : false;
+        return i == 0;
     }
 };
